dbf3336a3672d85f38fabc854c443e2e892f07ea
andresprimera <andresprimera@gmail.com>
2023-08-30 16:26:53 -0500

    Fix first button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,13 +8,20 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-10 col-md-offset-1 intro-text">
-                <div className="blob" />
+                <div
+                  className="blob"
+                  onClick={() => (window.location.href = "#about")}
+                />
                 <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a href="#about" className="btn btn-custom btn-lg page-scroll">
+                <a
+                  href="#about"
+                  className="btn btn-custom btn-lg page-scroll"
+                  style={{ zIndex: 10 }}
+                >
                   Conoce más
                 </a>{" "}
               </div>
